fca947db6a73fb952975ae37a41b1c66ac6cfa8f
Ram bhuyan <rambhuyan23@gmail.com>
2026-03-04 14:14:16 +0530

    Update service.py

--- service.py
+++ service.py
@@ -1,17 +1,7 @@
 from time import sleep
 from jnius import autoclass
-import os
 
-# Android Service components
-PythonService = autoclass('org.kivy.android.PythonService')
-mService = PythonService.mService
-
-def run_background_task():
-    print("ZARA Background Service Started...")
-    while True:
-        # Aapka background monitoring logic yahan chalega
-        # Example: checking for specific notifications or system states
-        sleep(10) 
-
-if __name__ == '__main__':
-    run_background_task()
+# Background mein service ko active rakhne ke liye
+while True:
+    print("ZARA Background Service is listening...")
+    sleep(15)
